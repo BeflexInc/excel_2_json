afeaff5b474676e8c2525502e26b433e9cf58bee
Jason Hwang <jasonh@beflex.co>
2020-08-17 13:54:08 +0900

    Converter done

---
+++ lib/main.py
@@ -0,0 +1,40 @@
+import json
+import openpyxl
+from openpyxl import load_workbook
+
+# excelFile = input("Excel File location :: ")
+
+loadedExcel = load_workbook('../assets/Beflex app Text resource.xlsx', data_only=True)
+
+loadedSheet = loadedExcel['Sheet1']
+
+targetLangCode = {'B': 'en-US', 'C': 'zh-CN', 'D': 'zh-TW', 'E': 'zh-TW', 'F': 'zh-HK', 'G': 'ko-KR', 'H': 'fr-FR', 'I': 'es-ES', 'J': 'it-IT', 'K': 'nl-NL', 'L': 'ja-JP'}
+langCodeKey = list(targetLangCode.keys())
+keyList = []
+valueList = []
+json_dict = {}
+
+# Column name : language code
+# B : EN
+# C : SCN
+# D : TCN
+# E : TW
+# F : HK
+# G : KR
+# H : FR
+# I : SP
+# J : IT
+# K : NL (dutch)
+# L : JP
+
+for row in loadedSheet['A']:
+    keyList.append(row.value)
+
+for element in langCodeKey:
+    for row in loadedSheet[element]:
+        valueList.append(row.value)
+    json_dict = dict(zip(keyList, valueList))
+    valueList = []
+    del (json_dict[keyList[0]])
+    with open('../assets/' + targetLangCode[element] + '.json', 'w', encoding='UTF-8') as outfile:
+        json.dump(json_dict, outfile, ensure_ascii=False)
