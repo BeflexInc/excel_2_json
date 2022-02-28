a5c6e5fbc7e3a55a90e614a3db9631c89612ade8
Taeho Hwang <hth225@gmail.com>
2022-02-28 21:52:09 +0900

    Apply OAuth, tool improvement

--- README.md
+++ README.md
@@ -1,12 +1,26 @@
 # Excel 2 json
-
+![sheetImage](https://lh3.ggpht.com/e3oZddUHSC6EcnxC80rl_6HbY94sM63dn6KrEXJ-C4GIUN-t1XM0uYA_WUwyhbIHmVMH=w300)
+구글 drive api 를 이용해서 구글 아이디로 로그인하고 편리하게 excel to json format 으로 convert 해주는 툴
+   
+## 🛠 Setup
+### Create credential 
+1. [Google cloud platform console](https://console.developers.google.com/home) 열고
+2. APIs & Services > Credentials 접근
+3. OAuth client ID 만들기
+4. Application type > Desktop application
+5. 이름 -> Beflex i18n converter
+6. 위 설정들 마치고 만든 OAuth client json 파일 다운로드
+7. 본 프로젝트의 lib 폴더 안에 credentials.json 으로 저장
+   
+### Install google client library
+> pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+   
+[참고 자료](https://developers.google.com/workspace/guides/create-credentials#oauth-client-id)
+   
 ## 🧑‍💻 How to use?
 1. python 가상환경을 사용하기 위해 terminal 에 해당 프로젝트 경로에 접근
 2. ```. venv/bin/activate``` 명령으로 가상환경 활성화
 3. ```python3 lib/main.py``` 로 실행
-4. 파일 경로 기재
-5. 변환된 파일은 assets 폴더에 있음
-
-### 🛠️ 새 언어가 추가됐을 때
-
-```targetLangCode``` 딕셔너리에 넣는 칼럼 하나씩 추가해주면 됨
+4. 구글 로그인
+5. 파일 경로 기재
+6. 기재한 프로젝트 i18n 경로로 변환된 파일 저장됨.
--- lib/main.py
+++ lib/main.py
@@ -1,36 +1,123 @@
-import json
+from __future__ import print_function
 from openpyxl import load_workbook
 
-excelFile = input("Excel File location : ").strip()
+import json
+import os.path
+import io
 
-loadedExcel = load_workbook(excelFile, data_only=True)
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
 
-# 활성화된 시트 가져오기
-loadedSheet = loadedExcel.active
+# If modifying these scopes, delete the file token.json.
+from googleapiclient.http import MediaIoBaseDownload
 
-# 새 언어 추가될때마다 주석 풀어주면 됨
-targetLangCode = {'B': loadedSheet['B1'].value, 'C': loadedSheet['C1'].value, 'D': loadedSheet['D1'].value,
-                  # 'E': loadedSheet['E1'].value, 'F': loadedSheet['F1'].value, 'G': loadedSheet['G1'].value,
-                  }
-langCodeKey = list(targetLangCode.keys())
-keyList = []
-valueList = []
-json_dict = {}
+SCOPES = ['https://www.googleapis.com/auth/drive']
 
-# 첫번째 column이 key 값.
-# Key 값 배열 만들기
-for row in loadedSheet['A']:
-    keyList.append(row.value)
+def downloadTextResourceFromDrive():
+    """Shows basic usage of the Drive v3 API.
+    Prints the names and ids of the first 10 files the user has access to.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
 
-for element in langCodeKey:
-    # Value 배열 만들기
-    for row in loadedSheet[element]:
-        valueList.append(row.value)
-    json_dict = dict(zip(keyList, valueList))
+    try:
+        service = build('drive', 'v3', credentials=creds)
+
+        # Call the Drive v3 API
+        results = service.files().list(
+            q="name = 'beflex_app_text_resource'",
+            fields="nextPageToken, files(id, name)",
+            includeItemsFromAllDrives=True,
+            supportsAllDrives=True,
+        ).execute()
+        items = results.get('files', [])
+
+        if not items:
+            print('No files found.')
+            return
+        print('Files:')
+        for item in items:
+            textResourceFileId = item['id']
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+
+        request = service.files().export_media(fileId=textResourceFileId,
+                                               mimeType='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        fh = io.FileIO('../assets/resource.xlsx', 'w+')
+        downloader = MediaIoBaseDownload(fh, request)
+        done = False
+        while done is False:
+            status, done = downloader.next_chunk()
+            print("Download %d%%." % int(status.progress() * 100))
+
+    except HttpError as error:
+        # TODO(developer) - Handle errors from drive API.
+        print(f'An error occurred: {error}')
+
+def convertExcelToJson():
+    projectResourcePath = ''
+    filesize = os.path.getsize('../assets/flutter_project_resource_path.txt')
+    if(filesize == 0):
+        path = input('flutter project i18n path :')
+        i18nLocation = open('../assets/flutter_project_resource_path.txt', 'w')
+        i18nLocation.write(path)
+        i18nLocation.close()
+        projectResourcePath = path
+    else:
+        projectResourcePath = open('../assets/flutter_project_resource_path.txt', 'r').read()
+
+    loadedExcel = load_workbook('../assets/resource.xlsx', data_only=True)
+
+    # 활성화된 시트 가져오기
+    loadedSheet = loadedExcel.active
+
+    # 새 언어 추가될때마다 주석 풀어주면 됨
+    targetLangCode = {'B': loadedSheet['B1'].value, 'C': loadedSheet['C1'].value, 'D': loadedSheet['D1'].value,
+                      # 'E': loadedSheet['E1'].value, 'F': loadedSheet['F1'].value, 'G': loadedSheet['G1'].value,
+                      }
+    langCodeKey = list(targetLangCode.keys())
+    keyList = []
     valueList = []
-    # 첫 번째 row 의 언어 코드 row 지우기
-    del (json_dict[keyList[0]])
-    # 변환
-    with open('./assets/' + str(targetLangCode[element]).lower() + '.json', 'w', encoding='UTF-8') as outfile:
-        json.dump(json_dict, outfile, ensure_ascii=False, indent='\t')
-        print('Done with ' + str(targetLangCode[element]).lower())
+    json_dict = {}
+
+    # 첫번째 column이 key 값.
+    # Key 값 배열 만들기
+    for row in loadedSheet['A']:
+        keyList.append(row.value)
+
+    for element in langCodeKey:
+        # Value 배열 만들기
+        for row in loadedSheet[element]:
+            valueList.append(row.value)
+        json_dict = dict(zip(keyList, valueList))
+        valueList = []
+        # 첫 번째 row 의 언어 코드 row 지우기
+        del (json_dict[keyList[0]])
+        # 변환
+        with open(projectResourcePath + '/' + str(targetLangCode[element]) + '.json', 'w', encoding='UTF-8') as outfile:
+            json.dump(json_dict, outfile, ensure_ascii=False, indent='\t')
+            print('Done with ' + str(targetLangCode[element]))
+
+def main():
+    downloadTextResourceFromDrive()
+    convertExcelToJson()
+
+if __name__ == '__main__':
+    main()
